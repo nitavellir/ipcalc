3961f1b726bde7e26878bf2ddc963daa49ab3f2b
rr <ryohamadapride@gmail.com>
2021-03-27 02:18:18 +0900

    int to ip

---
+++ README.md
@@ -0,0 +1,7 @@
+## IP → 数値変換
+
+* go run ipcalc.go 1 x.x.x.x(IP)
+
+## 数値 → IP変換
+
+* go run ipcalc.go 2 xxxxxxx(数値)
--- ipcalc.go
+++ ipcalc.go
@@ -17,6 +17,12 @@ func main() {
 			panic(err)
 		}
 		fmt.Println("IP:", input, ", INT:", res)
+	} else if opt == "2" {
+		res, err := intToIp(input)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("INT:", input, ", IP:", res)
 	}
 }
 
@@ -45,4 +51,34 @@ func ipToInt(input string) (int, error) {
 
 	res := block1*256*256*256 + block2*256*256 + block3*256 + block4
 	return res, nil
+}
+
+func intToIp(input string) (string, error) {
+	inputInt, _ := strconv.Atoi(input)
+
+	bin := fmt.Sprintf("%b", inputInt)
+	tmpNum := ""
+	resElemsStr := []string{}
+	for k, v := range bin {
+		tmpNum += string(v)
+		if k%8 == 7 {
+			resElemsStr = append(resElemsStr, tmpNum)
+			tmpNum = ""
+		}
+	}
+
+	resElems := make([]int64, len(resElemsStr))
+	for k, v := range resElemsStr {
+		elem, err := strconv.ParseInt(v, 2, 64)
+		if err != nil {
+			return "", err
+		}
+		resElems[k] = elem
+	}
+
+	if len(resElems) != 4 {
+		return "", errors.New(fmt.Sprintf("Invalid IP elements"))
+	}
+
+	return fmt.Sprintf("%d.%d.%d.%d", resElems[0], resElems[1], resElems[2], resElems[3]), nil
 }
